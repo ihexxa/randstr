79a74b176c5d473fc2fd935fb5041842218e1b0f
Hexxa <hexxa@outlook.com>
2021-01-06 22:18:03 +0800

    fix(seed): fix a seed bug, refine variable length generating, add slice APIs (#2)
    Co-authored-by: Jia He <jiah@nvidia.com>

--- README.md
+++ README.md
@@ -9,14 +9,17 @@ _Customizable random string generator in Go/Golang._
 ### Examples
 
 Document is [here](https://pkg.go.dev/github.com/ihexxa/randstr).
+
 **Note**: This library should NOT be used in security-critical system.
+
+# Generate random strings with numbers/alphabets/alnums/candidates:
 
 ```go
 import "github.com/ihexxa/randstr"
 
 // customized generator
 candidates := []string{"你", "好", "世", "界"}
-randStr := randstr.NewRandStr(candidates, true, 8)
+randStr := randstr.New(candidates)
 
 fmt.Println(randStr.Gen()) // output could be: 世世界你你你你好
 fmt.Println(randStr.Alphabets())
@@ -24,3 +27,37 @@ fmt.Println(randStr.Numbers())
 fmt.Println(randStr.Alnums())
 
 ```
+
+# Generate fixed-length random strings:
+
+```go
+import "github.com/ihexxa/randstr"
+
+candidates := []string{"你", "好", "世", "界"}
+randStr := randstr.New(candidates)
+randStr.SetLenFixed(true) // set sample size fixed
+randStr.SetLenMax(8) // set sample size to 8
+// above 3 lines can be replaced with randstr.NewRandstr(candidates, true, 8)
+
+fmt.Println(randStr.Gen())
+...
+
+```
+
+# Generate random slices:
+
+```go
+import "github.com/ihexxa/randstr"
+
+randStr := randstr.New([]string{"你", "好", "世", "界"})
+
+fmt.Println(randStr.GenSlice())
+fmt.Println(randStr.AlphabetSlice())
+fmt.Println(randStr.NumberSlice())
+fmt.Println(randStr.AlnumSlice())
+
+```
+
+### License
+
+MIT
--- generator.go
+++ generator.go
@@ -10,14 +10,19 @@ var (
 		"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 	}
 	numberSet = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	defaultMaxStrLen = 8
+	defaultIsLenFixed = false
 )
 
 // RandStr is random string generator
 type RandStr struct {
-	seed            int64
 	candidates      []string
 	sampleSizeFixed bool
 	sampleSizeMax   int
+}
+
+func New(candidates []string) *RandStr {
+	return NewRandStr(candidates, defaultIsLenFixed, defaultMaxStrLen)
 }
 
 // NewRandStr create a new generator
@@ -29,41 +34,80 @@ func NewRandStr(candidates []string, sampleSizeFixed bool, sampleSizeMax int) *R
 	}
 }
 
-func (g *RandStr) genBy(candidates []string) string {
-	rand.Seed(g.seed)
-
+func (g *RandStr) sample(candidates []string) string {
+	sampleSize := g.sampleSizeMax
+	if !g.sampleSizeFixed {
+		sampleSize = rand.Intn(g.sampleSizeMax) + 1
+	}
+	
 	randStr := ""
-	for i := 0; i < g.sampleSizeMax; i++ {
+	for i := 0; i < sampleSize; i++ {
 		randStr += candidates[rand.Intn(len(candidates))]
-		if i != 0 && !g.sampleSizeFixed && rand.Intn(2) == 1 { // at least sample once
-			break
-		}
 	}
 
 	return randStr
 }
 
+// SetLenFixed sets if randstr generates fixed size random strings
+func (g *RandStr) SetLenFixed(isFixed bool) {
+	g.sampleSizeFixed = isFixed
+}
+
+// SetLenMax sets the max length of random strings.
+// If sample size is fixed, it is the sample size.
+func (g *RandStr) SetLenMax(sampleSizeMax int) {
+	g.sampleSizeMax = sampleSizeMax
+}
+
 // Seed sets a seed to math.rand
 func (g *RandStr) Seed(seed int64) {
-	g.seed = seed
+	rand.Seed(seed)
 }
 
 // Gen generates a random string according to candidates
 func (g *RandStr) Gen() string {
-	return g.genBy(g.candidates)
+	return g.sample(g.candidates)
+}
+
+// GenSlice generates a slice of random strings according to candidates
+func (g *RandStr) GenSlice(length int) []string {
+	return g.genSlice(length, g.candidates)
+}
+
+func (g *RandStr) genSlice(length int, candidates []string) []string {
+	strs := make([]string, length, length)
+	for i := range strs {
+		strs[i] = g.sample(candidates)
+	}
+	return strs
 }
 
 // Alphabets generates a random string with alphabets
 func (g *RandStr) Alphabets() string {
-	return g.genBy(alphabetSet)
+	return g.sample(alphabetSet)
+}
+
+// AlphabetSlice generates a slice of random strings with alphabets
+func (g *RandStr) AlphabetSlice(length int) []string {
+	return g.genSlice(length, alphabetSet)
 }
 
 // Numbers generates a random string with numbers
 func (g *RandStr) Numbers() string {
-	return g.genBy(numberSet)
+	return g.sample(numberSet)
+}
+
+// NumberSlice generates a slice of random strings with numbers
+func (g *RandStr) NumberSlice(length int) []string {
+	return g.genSlice(length, alphabetSet)
 }
 
 // Alnums generates a random string with alphabets and numbers
 func (g *RandStr) Alnums() string {
-	return g.genBy(append(alphabetSet, numberSet...))
+	return g.sample(append(alphabetSet, numberSet...))
+}
+
+// AlnumSlice generates a slice of random strings with alphabets and numbers
+func (g *RandStr) AlnumSlice(length int) []string {
+	return g.genSlice(length, append(alphabetSet, numberSet...))
 }
